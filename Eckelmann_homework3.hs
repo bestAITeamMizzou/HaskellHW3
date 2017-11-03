c1c7bdc6ad839f94d20ae002632a11ca3ab4210a
FireLemons <shenyang64@gmail.com>
2017-11-03 16:32:37 -0500

    3/5 test done

--- Eckelmann_homework3.hs
+++ Eckelmann_homework3.hs
@@ -34,11 +34,25 @@ test_probs = do
   test_prob4
 
 test_prob1 :: IO()
-test_prob1 = undefined
+test_prob1 = hspec $ do
+  describe "prob1(parser)" $ do
+    context "when provided with invalid input" $ do
+      it "returns a PExp" $ do
+        prob1 "200 + - * /" `shouldBe` [Val 200, Plus, Minus, Mul, IntDiv]
 
 test_prob2 :: IO()
-test_prob2 = undefined
-
+test_prob2 = hspec $ do
+  describe "prob2(evaluation)" $ do
+    context "when provided with valid input" $ do
+      it "returns a Int" $ do
+        prob2 [Val 4, Val 2, IntDiv] `shouldBe` 2
+    context "when provided with syntactically incorrect input" $ do
+      it "throws an error" $ do
+        prob2 [Mul] `shouldThrow` errorCall "Bad Input."
+    context "when provided with syntactically incorrect input" $ do
+      it "throws an error" $ do
+        prob2 [Mul] `shouldThrow` errorCall "Cannot divide by zero!"
+		
 test_prob3 :: IO()
 test_prob3 = undefined
 

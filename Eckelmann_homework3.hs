aae24a1d76df9caeee2bd15f03e075f25b04ebb4
FireLemons <shenyang64@gmail.com>
2017-11-03 16:04:38 -0500

    added empty tests

--- Eckelmann_homework3.hs
+++ Eckelmann_homework3.hs
@@ -1,10 +1,16 @@
 {-# OPTIONS_GHC -fwarn-missing-signatures #-}
 {-# OPTIONS_GHC -fno-warn-tabs #-}
+--{-# LANGUAGE DeriveFoldable #-}
+--{-# LANGUAGE DeriveFunctor #-}
+--{-# LANGUAGE DeriveTraversable #-}
+--{-# LANGUAGE FlexibleInstances #-}
+--{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+--{-# LANGUAGE LambdaCase #-}
+
 module Homework3 where
 
 import Test.Hspec
 import RPNAST
-
 
 prob1    :: a
 prob1    = undefined
@@ -19,3 +25,18 @@ prob4    :: a
 prob4    = undefined
 
 -- Write your Hspec Tests below
+
+test_probs :: IO()
+test_probs = undefined
+
+test_prob1 :: IO()
+test_prob1 = undefined
+
+test_prob2 :: IO()
+test_prob2 = undefined
+
+test_prob3 :: IO()
+test_prob3 = undefined
+
+test_prob4 :: IO()
+test_prob4 = undefined
